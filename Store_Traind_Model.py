c1d7ef30159cdb12883268755a7ccce4a320693e
admantiumboy <admantiumboy@gmail.com>
2019-02-05 00:50:45 +0530

    Documentation is completed now Main function call from the testFunction.py

--- Store_Traind_Model.py
+++ Store_Traind_Model.py
@@ -2,6 +2,7 @@ import pickle
 import string
 import pandas as pd
 import sklearn
+
 news_df = pd.read_csv("input/uci-news-aggregator.csv", sep=",")
 news_df['CATEGORY'] = news_df.CATEGORY.map({'b': 1, 't': 2, 'e': 3, 'm': 4})
 news_df['TITLE'] = news_df.TITLE.map(
@@ -18,9 +19,9 @@ from sklearn.feature_extraction.text import CountVectorizer
 
 count_vector = CountVectorizer(stop_words='english')
 training_data = count_vector.fit_transform(X_train)
+print(training_data)
 
 from sklearn.naive_bayes import MultinomialNB
-
 naive_bayes = MultinomialNB()
 naive_bayes.fit(training_data, y_train)
 filename = 'News_finalized_model.sav'
--- TextRankProject.py
+++ TextRankProject.py
@@ -11,28 +11,64 @@ from sklearn.feature_extraction.text import CountVectorizer  # For Bag of words
 
 
 def classify(ranked_text):
+    """
+    Used for the classification of the sentences
+    :parameter ranked_text (list)
+    """
+    print(ranked_text)
+    news_df = pd.read_csv("input/uci-news-aggregator.csv", sep=",")
+    news_df['CATEGORY'] = news_df.CATEGORY.map({'b': 1, 't': 2, 'e': 3, 'm': 4})
+    news_df['TITLE'] = news_df.TITLE.map(
+        lambda x: x.lower().translate(str.maketrans('', '', string.punctuation))
+    )
+    from sklearn.model_selection import train_test_split
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        news_df['TITLE'],
+        news_df['CATEGORY'],
+        random_state=1
+    )
     from sklearn.feature_extraction.text import CountVectorizer
+
     count_vector = CountVectorizer(stop_words='english')
+    training_data = count_vector.fit_transform(X_train)
     testing_data = count_vector.transform(ranked_text)
+
+    from sklearn.naive_bayes import MultinomialNB
+    naive_bayes = MultinomialNB()
+    naive_bayes.fit(training_data, y_train)
+    filename = 'News_finalized_model.sav'
+    pickle.dump(naive_bayes, open(filename, 'wb'))
     filename = 'News_finalized_model.sav'
     model = pickle.load(open(filename, 'rb'))
 
     predictions_nb = model.predict(testing_data)
     print(predictions_nb)
-    exit(1)
 
 
 # function to read CSV dataset
-def read_csv():
-    # print("input/twitter/five_ten.csv")
-    # data_path = input("Enter Dataset Path")
-    data_path = "input/twitter/twentyfive_thirty.csv"
-    data_threads = pd.read_csv(data_path, encoding='latin-1')
+def read_csv(FileName):
+    """
+    This Function will read the CSV input file
+    :param str FileName : The path to the input file
+    :rtype dataframe : return the dataframe of the pandas library
+    :return : return the pandas dataframe of the input file
+
+    """
+    data_threads = pd.read_csv(FileName, encoding='latin-1')
     return data_threads
 
 
 # Function to remove duplicates
 def remove_duplicate(data_threads):
+    """
+        This Function will remove duplicates of the dataframe.
+        :param dataframe data_threads: pandas dataframe having columns thread_number and text
+        :rtype dataframe : return the dataframe of the pandas library
+        :return : return the pandas dataframe after removed the duplicates
+
+    """
+
     final_data = data_threads.drop_duplicates(subset={"thread_number", "text", "retweets", "likes", "replies"})
     return final_data
 
@@ -44,6 +80,12 @@ def get_needed_data(final_data):
 
 
 def unwanted_text_removal(final_text):
+    """
+           This Function will remove duplicates of the dataframe.
+           :param final_text : pandas dataframe having columns thread_number and text
+           :rtype dataframe : return the dataframe of the pandas library
+           :return : return the pandas dataframe after removed HTML tags, Removing Punctuations
+       """
     import re
     temp = []
     for sentence in final_text:
@@ -60,6 +102,11 @@ def unwanted_text_removal(final_text):
 
 
 def combine_words_to_sentence(final_text):
+    """
+    make sentences from words
+    :param final_text: list of words
+    :return: merged sentences
+    """
     temp = []
     for row in final_text:
         sequ = ''
@@ -70,12 +117,25 @@ def combine_words_to_sentence(final_text):
 
 
 def vect_conversion(final_text):
+    """
+    Convert the Text into the Vectorized form using Counter Vector max_features=5000
+    :param final_text: list of words or sentences
+    :return: vectorized form of the text
+    """
     count_vect = CountVectorizer(max_features=5000)
     vect_data = count_vect.fit_transform(final_text)
     return vect_data
 
 
-def summerized_text(final_text, vect_data):
+def text_rank(final_text, vect_data, no_of_line=5):
+    """
+    Implemented Text Rank algorithm, does the ranking of the statements as given input
+    :rtype: dataframe
+    :param final_text: preprocessed text data
+    :param vect_data: vectored data of the final_text
+    :param no_of_line: no of summarized line, default value is 5
+    :return: ranked sentences as per text rank algorithm
+    """
     sim_mat = np.zeros([len(final_text), len(final_text)])
     from sklearn.metrics.pairwise import cosine_similarity
     for i in range(len(final_text)):
@@ -90,58 +150,60 @@ def summerized_text(final_text, vect_data):
 
     ranked_sentences = sorted(((scores[i], s) for i, s in enumerate(final_text)), reverse=True)
 
-    no_of_line = 5
-    # print(final)
     return ranked_sentences[:no_of_line]
 
 
 # summarize all complete thread
-def summarization(text):
+def summarization(text,No_of_sentences=5):
+    """
+    takes the dataframe and return top N sentences (Summarized Text)
+    :param text:  pandas dataframe having columns thread_number and text
+    :return: N ranked sentences
+    """
     final_text = unwanted_text_removal(text)
     final_text = combine_words_to_sentence(final_text)
     vect_data = vect_conversion(final_text)
-    summarized_data = summerized_text(final_text, vect_data)
+    summarized_data = text_rank(final_text, vect_data)
     ranked_text = []
-    for i in range(0, 5):
+    for i in range(0, No_of_sentences):
         ranked_text.append(summarized_data[i][1])
-    classify(ranked_text)
-    # print(summarized_data)
+
+    return ranked_text
 
 
-# calling of functions
-data_thread = read_csv()
-data = remove_duplicate(data_thread)
-thread_number, text = get_needed_data(data)
+def text_rank_output(FilePath):
+    """
+    Need specific type of dataset which contains
 
-count = 0
-g_count = 0
-redundent = thread_number[0]
-one_complete_thred = []
+    Thread_number - No of that conversation
 
-for thread_iterator in thread_number:
-    if thread_iterator == redundent:
-        one_complete_thred.insert(count, text[g_count])
-        count += 1
-        g_count += 1
+    Text - Conversation of that Thread
 
-    else:
-        # print(one_complete_thred)
-        summarization(one_complete_thred)
-        one_complete_thred.clear()
-        count = 0
-        one_complete_thred.insert(count, text[g_count])
-        count += 1;
-        g_count += 1
-        redundent = thread_iterator
+    :param str FilePath: path of the input File
+    :return:
+    """
+    data_thread = read_csv(FilePath)
+    data = remove_duplicate(data_thread)
+    thread_number, text = get_needed_data(data)
 
-# for i in range(no_threads):
-#     merger_list.append(text[i])
+    count = 0
+    g_count = 0
+    redundent = thread_number[0]
+    one_complete_thred = []
 
+    for thread_iterator in thread_number:
+        if thread_iterator == redundent:
+            one_complete_thred.insert(count, text[g_count])
+            count += 1
+            g_count += 1
 
-#
-# final_text = unwanted_text_removal(text)
-# final_text = combine_words_to_sentence(final_text)
-# vect_data=vect_conversion(final_text)
-# summerized_text(final_text,vect_data)
-#
-#
+        else:
+            # print(one_complete_thred)
+            top_senteces=summarization(one_complete_thred)
+            classify(top_senteces)
+            one_complete_thred.clear()
+            count = 0
+            one_complete_thred.insert(count, text[g_count])
+            count += 1;
+            g_count += 1
+            redundent = thread_iterator
